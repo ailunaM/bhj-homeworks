d2d063a18322d99d70d2fd864f18f525c019419d
ailunaM <ajlunaravsanbekova@gmail.com>
2024-10-24 00:58:24 +0600

    additional countdown task№2

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -11,19 +11,19 @@ let intervalId = setInterval(() => {
 const countdown = document.getElementById('countdown');
 
 let intervalIdClear = setInterval(() => {
-  const timeText = countdown.textContent; 
+  const timeText = countdown.textContent;
   const [h, m, s] = timeText.split(':');
-  const ms = new Date().setHours(h, m, s); 
+  const ms = new Date().setHours(h, m, s);
 
   const date = new Date(ms - 1000).toLocaleTimeString('ru-Ru', {
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
-  }); 
+  });
   countdown.textContent = date;
 
   if (date.includes('00:00:00')) {
     clearInterval(intervalIdClear);
-    alert('Вы победили в конкурсе!');
+    location = 'https://hellokittymarket.ru/';
   }
 }, 1000);
