0c9ac3e2d05e063cc49d72bc43dfa440a3bbb165
ailunaM <ajlunaravsanbekova@gmail.com>
2024-10-24 00:44:02 +0600

    additional countdown task done

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,9 +1,29 @@
-const timer = document.getElementById('timer'); 
-console.dir(timer);
+const timer = document.getElementById('timer');
+
 let intervalId = setInterval(() => {
   timer.textContent -= 1;
   if (timer.textContent == 0) {
     clearInterval(intervalId);
+    alert('Вы победили в конкурсе!');
+  }
+}, 1000);
+
+const countdown = document.getElementById('countdown');
+
+let intervalIdClear = setInterval(() => {
+  const timeText = countdown.textContent; 
+  const [h, m, s] = timeText.split(':');
+  const ms = new Date().setHours(h, m, s); 
+
+  const date = new Date(ms - 1000).toLocaleTimeString('ru-Ru', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  }); 
+  countdown.textContent = date;
+
+  if (date.includes('00:00:00')) {
+    clearInterval(intervalIdClear);
     alert('Вы победили в конкурсе!');
   }
 }, 1000);
