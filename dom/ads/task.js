e2559b4659fb9d9e3ad3eeb69eade298e46ed759
ailunaM <ajlunaravsanbekova@gmail.com>
2024-11-10 20:12:09 +0600

    task №2 done

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,9 +1,9 @@
 const rotators = document.getElementsByClassName('rotator');
 const arrayRotators = Array.from(rotators);
 
-arrayRotators.forEach((rotator) => {
+arrayRotators.forEach((rotator, i) => {
   const rotatorCase = Array.from(rotator.getElementsByClassName('rotator__case'));
-  let i = 0;
+
   rotator.textContent = rotatorCase[i].textContent
   setInterval(() => {
     i++
