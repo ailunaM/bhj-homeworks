ce8792270d74ef8f7bdced83e550fb14c1a16de7
ailunaM <ajlunaravsanbekova@gmail.com>
2024-11-15 21:22:11 +0600

    found better solution for ads

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -3,14 +3,15 @@ const arrayRotators = Array.from(rotators);
 
 arrayRotators.forEach((rotator, i) => {
   const rotatorCase = Array.from(rotator.getElementsByClassName('rotator__case'));
-
-  rotator.textContent = rotatorCase[i].textContent
+  let index = 0;
+  // rotator.textContent = rotatorCase[i].textContent
   setInterval(() => {
-    i++
-    if(i >= rotatorCase.length){
-        i = 0;
-    }
-    rotator.textContent = rotatorCase[i].textContent
+    index = (index + 1) %  rotatorCase.length
+    // i++
+    // if(i >= rotatorCase.length){
+    //     i = 0;
+    // }
+    rotator.textContent = rotatorCase[index].textContent
   }, 1000);
 });
 
