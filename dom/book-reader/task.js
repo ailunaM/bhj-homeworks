df80608017f54db9a78087291e11767f8c094e3b
ailunaM <ajlunaravsanbekova@gmail.com>
2024-11-18 01:18:27 +0600

    fix bug

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,22 +1,21 @@
 const fontSize = Array.from(document.getElementsByClassName('font-size'));
 const book = Array.from(document.getElementsByClassName('book'))[0];
-fontSize.forEach((A, i) => {
+fontSize.forEach((A) => {
   A.addEventListener('click', (e) => {
     e.preventDefault();
-    let data = A.getAttribute('data-size');
+
     fontSize.forEach((element) => {
-        element.classList.remove('font-size_active')
-    })
+      element.classList.remove('font-size_active');
+    });
+    A.classList.add('font-size_active');
+    book.classList.remove('book_fs-small', 'book_fs-big');
+
+    let data = A.getAttribute('data-size');
+
     if (data === 'small') {
-      A.classList.add('font-size_active');
       book.classList.add('book_fs-small');
     } else if (data === 'big') {
-      A.classList.add('font-size_active');
       book.classList.add('book_fs-big');
-    } else {
-      A.classList.add('font-size_active');
-      book.classList.remove('book_fs-small');
-      book.classList.remove('book_fs-big');
     }
   });
 });
