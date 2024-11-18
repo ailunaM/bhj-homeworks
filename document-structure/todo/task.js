c317ae78932d0dfeacea637080f4bc2615ca03b3
ailunaM <ajlunaravsanbekova@gmail.com>
2024-11-19 01:22:29 +0600

    task№2

---
+++ document-structure/todo/task.js
@@ -0,0 +1,31 @@
+const taskForm = document.getElementById('tasks__form');
+const taskInput = document.getElementById('task__input');
+
+taskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const tasksList = document.getElementById('tasks__list');
+  const taskText = taskInput.value;
+  //   let div = document.createElement('div');
+  //   div.textContent = taskText;
+  //   tasksList.appendChild(div);
+
+  const taskHtml = `
+<div class="task">
+                      <div class="task__title">
+                       ${taskText}
+                      </div>
+                      <a href="#" class="task__remove">&times;</a>
+ </div>
+`;
+  tasksList.insertAdjacentHTML('beforeend', taskHtml);
+  taskInput.value = '';
+  taskInput.focus();
+
+  document.querySelectorAll('.task__remove').forEach((removeTask) => {
+    removeTask.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      removeTask.parentElement.remove();
+    });
+  });
+});
