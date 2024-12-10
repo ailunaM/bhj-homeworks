a74937777bb176de21d2ab65515be90b0bb1c6f3
ailunaM <ajlunaravsanbekova@gmail.com>
2024-12-11 02:24:39 +0600

    fix bug task№1

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,18 +1,32 @@
 const hasTooltips = Array.from(document.getElementsByClassName('has-tooltip'));
+let activeTooltip = null;
 
 hasTooltips.forEach((hasTooltip) => {
   hasTooltip.addEventListener('click', (e) => {
     e.preventDefault();
-    const tooltip = document.querySelector('.tooltip');
-    if (tooltip) {
-      tooltip.remove();
-    }
-    const title = hasTooltip.getAttribute('title');
-    let html = `
-       <div class="tooltip tooltip_active">
-      ${title}
-    </div>`;
 
-    hasTooltip.insertAdjacentHTML('beforeend', html);
+    const title = hasTooltip.getAttribute('title');
+    const existingTooltip = document.querySelector('.tooltip');
+
+    if (existingTooltip) {
+      if (activeTooltip === hasTooltip) {
+        existingTooltip.remove();
+        activeTooltip = null;
+        return;
+      } else {
+        existingTooltip.remove();
+      }
+    }
+
+    const tooltip = document.createElement('div');
+    tooltip.className = 'tooltip tooltip_active';
+    tooltip.textContent = title;
+    document.body.appendChild(tooltip);
+
+    const linkRect = hasTooltip.getBoundingClientRect();
+    tooltip.style.left = `${linkRect.left}px`;
+    tooltip.style.top = `${linkRect.bottom}px`;
+
+    activeTooltip = hasTooltip;
   });
 });
