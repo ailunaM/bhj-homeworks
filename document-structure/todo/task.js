451b32463370df607a4e3a3229228a0e974ac099
ailunaM <ajlunaravsanbekova@gmail.com>
2024-12-10 23:54:23 +0600

    fix bug task№2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,10 +1,18 @@
 const taskForm = document.getElementById('tasks__form');
 const taskInput = document.getElementById('task__input');
 
+function removeTaskFunc(e) {
+  e.preventDefault();
+  e.target.parentElement.remove();
+}
+
 taskForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const tasksList = document.getElementById('tasks__list');
   const taskText = taskInput.value;
+  if (taskText.trim() === '') {
+    return;
+  }
 
   const taskHtml = `
 <div class="task">
@@ -12,15 +20,11 @@ taskForm.addEventListener('submit', (e) => {
     <a href="#" class="task__remove">&times;</a>
  </div>
 `;
+
   tasksList.insertAdjacentHTML('beforeend', taskHtml);
+  let newTask = tasksList.lastElementChild;
+  let removeBtn = newTask.querySelector('.task__remove');
+  removeBtn.addEventListener('click', removeTaskFunc);
   taskInput.value = '';
   taskInput.focus();
-
-  document.querySelectorAll('.task__remove').forEach((removeTask) => {
-    removeTask.addEventListener('click', (e) => {
-      e.preventDefault();
-
-      removeTask.parentElement.remove();
-    });
-  });
 });
